58e131905d6adef374e7976ee130e0118adf4dbc
Stephen Colebourne <scolebourne@joda.org>
2013-08-12 12:07:20 +0100

    Fix Javadoc

--- src/main/java/org/joda/money/Money.java
+++ src/main/java/org/joda/money/Money.java
@@ -319,7 +319,7 @@ public final class Money implements BigMoneyProvider, Comparable<BigMoneyProvide
     /**
      * Parses an instance of {@code Money} from a string.
      * <p>
-     * The string format is '<currencyCode> <amount>'.
+     * The string format is '$currencyCode $amount'.
      * The currency code must be a valid three letter currency.
      * The amount must match the regular expression {@code [+-]?[0-9]*[.]?[0-9]*}.
      * This matches the output from {@link #toString()}.
