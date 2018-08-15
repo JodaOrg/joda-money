ece4d47786d17415bcc278db78a59f4062d62f2b
Stephen Colebourne <scolebourne@joda.org>
2018-08-15 19:15:34 +0100

    Release site directly via git
    mvn site-deploy not working and is very slow

---
+++ .github/website.sh
@@ -0,0 +1,24 @@
+
+echo "## setup..."
+git config --global user.name "Stephen Colebourne (CI)"
+git config --global user.email "scolebourne@joda.org"
+cd target
+
+echo "## clone..."
+git clone https://${GITHUB_TOKEN}@github.com/JodaOrg/jodaorg.github.io.git
+cd jodaorg.github.io
+git status
+
+echo "## copy..."
+rm -rf joda-money/
+cp -R ../site joda-money/
+
+echo "## update..."
+git add -A
+git status
+git commit --message "Update joda-money from Travis: $TRAVIS_BUILD_NUMBER"
+
+echo "## push..."
+git push origin master
+
+echo "## done"
